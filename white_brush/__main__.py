2bdbce90517282a77d2477e2664053b17637bdfe
philipp <philipphofer@gmx.net>
2018-05-08 14:16:10 +0200

    calculate representative colors from foreground pixels

--- white_brush/__main__.py
+++ white_brush/__main__.py
@@ -1,4 +1,6 @@
 import argparse
+from white_brush import calc_colors
+import numpy as np
 
 
 def parse_args():
@@ -9,9 +11,13 @@ def parse_args():
 
 
 def main():
-    args = parse_args()
+    input = np.array(
+        [[0, 10, 30], [10, 15, 12], [8, 6, 4], [56, 50, 45], [45, 2, 45], [0, 0, 0], [10, 56, 4], [5, 7, 9],
+         [1, 4, 7], [1, 54, 8], [9, 65, 4], [6, 5, 47], [5, 78, 5], [6, 1, 54], [6, 57, 45], [1, 1, 1]])
+    calc_colors.choose_representative_colors(input)
+    # args = parse_args()
 
-    print("Input: {}, Output: {}".format(args.input_file, args.output_file))
+    # print("Input: {}, Output: {}".format(args.input_file, args.output_file))
 
 
 if __name__ == "__main__":
