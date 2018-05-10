39463e59ee28604d37760f63d71b2d5c8161e01b
Christoph B <shynixn@gmail.com>
2018-05-10 13:42:58 +0200

    Fixed tests.

--- white_brush/commands/enhance_command.py
+++ white_brush/commands/enhance_command.py
@@ -23,29 +23,46 @@ class EnhanceCommand:
             self.__enhance_file_or_directory__(file, 0, enhance_configuration)
 
     def __enhance_file_or_directory__(self, file, counter, enhance_configuration):
+        """
+        Iterates the files and folders to match correct files which should be enhanced. Also increments file names and matches optional masks.
+        :param file: file or directory
+        :param counter: subdirectory counter
+        :param enhance_configuration: configuration
+        """
         if os.path.isdir(file):
             if enhance_configuration.recursive or counter == 0:
                 for sub_file in os.listdir(file):
                     self.__enhance_file_or_directory__(file + "/" + sub_file, counter + 1, enhance_configuration)
             return
+
         if enhance_configuration.replace_files:
             self.__enhance_file__(file, file, enhance_configuration)
         else:
             filename = pathlib.Path(file).stem
             extension = pathlib.Path(file).suffix
 
-            target_file = enhance_configuration.target_file_mask.replace("{name}", filename).replace("{extension}",
-                                                                                                     extension)
+            target_file = os.path.dirname(file) + "/" + enhance_configuration.target_file_mask.replace("{name}",
+                                                                                                       filename).replace(
+                "{extension}",
+                extension)
             counter = 1
+
             while os.path.exists(target_file):
-                target_file = enhance_configuration.target_file_mask.replace("{name}", filename).replace("{extension}",
-                                                                                                         " (" + counter + ")" + extension)
+                target_file = os.path.dirname(file) + "/" + enhance_configuration.target_file_mask.replace("{name}",
+                                                                                                           filename).replace(
+                    "{extension}", " (" + counter + ")" + extension)
 
             self.__enhance_file__(file, target_file, enhance_configuration)
 
     def __enhance_file__(self, source_file, target_file, enhance_configuration):
+        """
+        Enhances the given source file to the target_file if it exists.
+        :param source_file: source file
+        :param target_file:  target file
+        :param enhance_configuration:  configuration
+        """
         if not os.path.exists(source_file):
-            print("Input '" + source_file + "' does not exist.")
+            print("Input '" + source_file + "' does not exist. whitebrush --help")
             return
 
         if os.path.exists(target_file):
