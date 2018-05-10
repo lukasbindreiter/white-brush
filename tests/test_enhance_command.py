8a14c57ca4ec24ec478777a973ff39b0fb93f84c
Christoph B <shynixn@gmail.com>
2018-05-10 13:19:10 +0200

    Added enhancement tests.

--- tests/test_enhance_command.py
+++ tests/test_enhance_command.py
@@ -1,6 +1,8 @@
+import pathlib
 import unittest
 
 from white_brush.commands.enhance_command import EnhanceCommand
+from white_brush.entities.color_configuration import ColorConfiguration
 from white_brush.entities.enhancement_configuration import EnhancementConfiguration
 
 
@@ -25,6 +27,75 @@ class TestEnhanceCommand(unittest.TestCase):
         # Assert
         self.assertTrue(mocked_enhance_service.called, True)
 
+    def test_execute_given_multiple_files_enhancement_configuration_should_call_service(self):
+        # Arrange
+        files = ['../.pytest_cache/testfile1.png','../.pytest_cache/testfile2.png','../.pytest_cache/testfile3.png']
+        mocked_enhance_service = MockedEnhanceService()
+        class_under_test = EnhanceCommand(mocked_enhance_service)
+        enhance_configuration = EnhancementConfiguration()
+
+        # Act
+        for i in range(1,3):
+            f = open(files[i], "w+")
+            f.write("Hello World")
+            f.close()
+
+        class_under_test.execute(files, enhance_configuration)
+
+        # Assert
+        self.assertTrue(mocked_enhance_service.called, True)
+        self.assertEqual(mocked_enhance_service.called_counter, 3)
+
+    def test_execute_given_files_and_directories_enhancement_configuration_should_call_service(self):
+        # Arrange
+        files = ['../.pytest_cache/testfile1.png', '../.pytest_cache/testdirectory']
+        mocked_enhance_service = MockedEnhanceService()
+        class_under_test = EnhanceCommand(mocked_enhance_service)
+        enhance_configuration = EnhancementConfiguration()
+
+        # Act
+        pathlib.Path(files[1]).mkdir(parents=True, exist_ok=True)
+        f = open(files[1] + "/sample.jpg", "w+")
+        f.write("Hello World")
+        f.close()
+
+        class_under_test.execute(files, enhance_configuration)
+
+        # Assert
+        self.assertTrue(mocked_enhance_service.called, True)
+        self.assertEqual(mocked_enhance_service.called_counter, 2)
+
+    def test_execute_given_files_and_subdirectories_configuration_should_call_service(self):
+        # Arrange
+        files = ['../.pytest_cache/testfile1.png', '../.pytest_cache/testdirectory']
+        mocked_enhance_service = MockedEnhanceService()
+        class_under_test = EnhanceCommand(mocked_enhance_service)
+        enhance_configuration = EnhancementConfiguration()
+        enhance_configuration.recursive = True
+        enhance_configuration.foreground_color = "#fore"
+        enhance_configuration.background_color = "#back"
+
+        expected_foreground = "#fore"
+        expected_background = "#back"
+
+        # Act
+        pathlib.Path(files[1]).mkdir(parents=True, exist_ok=True)
+        f = open(files[1] + "/sample.jpg", "w+")
+        f.write("Hello World")
+        f.close()
+        pathlib.Path(files[1] + "/subtestdirectory").mkdir(parents=True, exist_ok=True)
+        f = open(files[1] + "/subtestdirectory/subsample.jpg", "w+")
+        f.write("Hello World")
+        f.close()
+
+        class_under_test.execute(files, enhance_configuration)
+
+        # Assert
+        self.assertTrue(mocked_enhance_service.called, True)
+        self.assertEqual(mocked_enhance_service.called_counter, 3)
+        self.assertEqual(mocked_enhance_service.called_color_configuration.foreground_color, expected_foreground)
+        self.assertEqual(mocked_enhance_service.called_color_configuration.background_color, expected_background)
+
     # endregion
 
 
@@ -34,10 +105,12 @@ class MockedEnhanceService:
     def __init__(self):
         self.called = False
         self.called_counter = 0
+        self.called_color_configuration = ColorConfiguration()
 
     def enhance_file(self, input_file_name, output_file_name, color_configuration):
         self.called = True
         self.called_counter += 1
+        self.called_color_configuration = color_configuration
 
 
 if __name__ == '__main__':
