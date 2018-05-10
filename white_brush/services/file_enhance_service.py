909a0d0aa606ec1ad2f537f83cb71482250dec37
Christoph B <shynixn@gmail.com>
2018-05-10 12:19:53 +0200

    Implemented template command.

--- white_brush/services/file_enhance_service.py
+++
@@ -1,5 +0,0 @@
-class FileEnhanceService:
-
-    def enhance_file(self, input_file_name, output_file_name, enhancement_configuration):
-        # TODO: Implement the actual enhancing.
-        return
