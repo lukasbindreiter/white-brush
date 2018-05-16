46359d18ab7fa0713348903fdf1b51a55ad5124f
Lukas Bindreiter <lukas.bindreiter@outlook.com>
2018-05-16 22:42:19 +0200

    colorspace conversion different array shapes

--- tests/colors/test_color_utils.py
+++ tests/colors/test_color_utils.py
@@ -13,11 +13,22 @@ class TestColorExtraction:
         """
         rgb_color = np.array([10, 20, 30], np.uint8)
         hsv_color = np.array([105, 170, 30], np.uint8)
-        rgb_color_image = np.tile(rgb_color, 5 * 5).reshape(5, 5, 3)
-        hsv_color_image = np.tile(hsv_color, 5 * 5).reshape(5, 5, 3)
 
         assert_allclose(rgb_to_hsv(rgb_color), hsv_color)
-        assert_allclose(rgb_to_hsv(rgb_color_image), hsv_color_image)
 
         assert_allclose(hsv_to_rgb(hsv_color), rgb_color)
-        assert_allclose(hsv_to_rgb(hsv_color_image), rgb_color_image)
+
+    def test_colorspace_conversion_input_shapes(self):
+        """
+        Test that colorspace conversion works with input arrays of
+        different shapes
+        """
+        rgb_color = np.array([10, 20, 30], np.uint8)
+        rgb_2D = np.tile(rgb_color, 5).reshape(5, 3)
+        rgb_3D = np.tile(rgb_color, 5 * 5).reshape(5, 5, 3)
+
+        def there_and_back_again(color_arr):
+            return hsv_to_rgb(rgb_to_hsv(color_arr))
+
+        assert_allclose(there_and_back_again(rgb_2D), rgb_2D)
+        assert_allclose(there_and_back_again(rgb_3D), rgb_3D)
