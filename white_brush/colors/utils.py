8035fbcd80bdb0af481d956ed640f959b2e5adcc
Christoph B <shynixn@gmail.com>
2018-05-19 22:19:47 +0200

    Added the parse_color method to colors.utils.

--- white_brush/colors/utils.py
+++ white_brush/colors/utils.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import webcolors
 
 
 def rgb_to_hsv(color: np.ndarray) -> np.ndarray:
@@ -44,10 +45,41 @@ def hsv_to_rgb(color):
 
 def __convert_color__(color, conversion_code):
     assert color.dtype == np.uint8
-    # color needs to be of shape (X, Y, 3) for the opencv cvtColor
+    # color needs to be of shape (X, Y, 3) for the opencv cvtColorexception
     # functions. Therefore if it is not in that shape, reshape it first
     # and then restore the original shape later on
     orig_shape = color.shape
     if color.ndim != 3:
         color = color.reshape(-1, 1, 3)
     return cv2.cvtColor(color, conversion_code).reshape(*orig_shape)
+
+
+def parse_color(color):
+    """
+    Parses the given color text to rgb values in format (255,255,255).
+
+    Args:
+        color: color text to be parsed in format 'red', '#728599', and 255,255,255
+
+    Returns:
+        Returns the parsed color as rgb value or None if not parseable.
+    """
+    try:
+        color = webcolors.name_to_rgb(color)
+        return color.red, color.green, color.blue
+    except ValueError:
+        pass
+
+    try:
+        color = webcolors.hex_to_rgb(color)
+        return color.red, color.green, color.blue
+    except ValueError:
+        pass
+
+    try:
+        data = color.split(",")
+        return int(data[0]), int(data[1]), int(data[2])
+    except Exception:
+        pass
+
+    return None
