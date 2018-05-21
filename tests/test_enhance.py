4d24bc8640013ee0edd81c90408f7bff98e9a370
Lukas Bindreiter <lukas.bindreiter@outlook.com>
2018-05-21 13:10:45 +0200

    Test enhance service

--- tests/test_enhance.py
+++ tests/test_enhance.py
@@ -1,10 +1,8 @@
-from unittest.mock import Mock
-
 from pytest_mock import MockFixture
 
 from tests.resources import get_test_image
-from white_brush.services import enhance_service
 from white_brush.enhance import enhance
+from white_brush.services import enhance_service
 
 
 class TestEnhance:
@@ -19,21 +17,15 @@ class TestEnhance:
 
 class TestEnhanceService:
     def test_enhance_service(self, mocker: MockFixture):
+        # mock the io module
         name, img = get_test_image()
-
-        def mock_io_read(filename):
-            return img
-
-        def mock_io_write(filename, img):
-            pass
-
-        mocker.patch("enhance_service.io.read_image", mock_io_read)
-        mocker.patch("enhance_service.enhance", lambda x: x)
-        mocker.patch("enhance_service.io.write_image", mock_io_write)
+        mock_io = mocker.patch("white_brush.io")()
+        mock_io.read_image.return_value = img
+        enhance_service.io = mock_io
 
         service = enhance_service.EnhanceService()
-        service.enhance_file("input.jpg", "output.jpg")
+        service.enhance_file("input.jpg", "output.jpg", None)
 
-        enhance_service.io.read_image.assert_called_once_with("input.jpg")
-        enhance_service.io.write_image.assert_called_once()
-        enhance_service.enhance.assert_called_once()
+        mock_io.read_image.assert_called_with("input.jpg")
+        mock_io.write_image.assert_called_once()
+
